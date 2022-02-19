f50cb9daee84ceead3f65853b87ba7659194136f
Nirav Pakala <nirav@ucsb.edu>
2022-02-19 12:43:02 -0800

    first part wokrs

--- Lab3/branch_predict_onelevel.v
+++ Lab3/branch_predict_onelevel.v
@@ -9,9 +9,10 @@ module btbuff #(parameter ROWS = 32'h00000020)  //32 entries , 2^5
 	input Clk, 
 	input Rst, 
 	input [1:0] statein, // latest state bit from branch history table
-	output [31:0] PCPredict, // the next PC to predict, PC + 4 if no brnach predicted
-	output prediction // control signal, prediction lets hazard unit know if predicted a branch or not
-) 
+	output reg [31:0] PCPredict, // the next PC to predict, PC + 4 if no brnach predicted
+	output reg prediction, // control signal, prediction lets hazard unit know if predicted a branch or not
+	output reg btbhit
+) ;
 
 	// states for FSM
 	parameter TAKEN  = 2'b00, // strongly taken
@@ -22,12 +23,27 @@ module btbuff #(parameter ROWS = 32'h00000020)  //32 entries , 2^5
 	// branch target buffer table
 	reg [66:0] buff[0:ROWS-1];  // 1 Valid Bit, 32 PC bits, 32 PC Prediction bits, 2 state bits
 
+	integer i;
+	always @(posedge Rst)  
+	begin		
+		btbhit <= 0;
+		prediction <= 0;
+		PCPredict <= 32'b0;
+
+		for (i=0;i<ROWS;i=i+1)
+		begin
+			buff[i] <= 67'b0; //on a reset, default all to Strongly Not taken
+		end
+
+	end
+
+
 	// index into the branch target buffer which has 2^5 entries (lower 5 bits of PC not including byte offset)
 	wire [4:0] buff_offset; // offset for getting prediction (execute stage) 
 	wire [4:0] buff_offset_current; // offset updating entry (fetch stage)
 
-	assign buff_offset <= PC[6:2]; // lower 5 bits from exec stage PC
-	assign buff_offset_current <= PC_current[6:2]; // lower 5 bits from fetch stage PC
+	assign buff_offset = PC[6:2]; // lower 5 bits from exec stage PC
+	assign buff_offset_current = PC_current[6:2]; // lower 5 bits from fetch stage PC
 
 	// if there was a branch instruction, need to update the corresponding entry
 	always @(posedge Branch)
@@ -49,16 +65,18 @@ module btbuff #(parameter ROWS = 32'h00000020)  //32 entries , 2^5
 			
 			// tell the hazard unit we predicted a branch taken in case we need to flush
 			prediction <= 1;
+			btbhit <= 1;
 			
 		end
 		// otherwise (we didn't find the branch in the buffer)
 		else
 		begin
 			// just go to PC + 4
-			PCPredict <= PC + 32'b4;
+			//PCPredict <= PC + 32'b4;
 
 			// tell the hazard unit we predicted a branch not taken in case we need to flush
 			prediction <= 0;
+			btbhit<=0;
 		end
 	end
 
@@ -72,7 +90,7 @@ module bht #(parameter ROWS = 32'h00000080) // 128 entries, 2^7
 	input BranchTaken, // control signal from decode stage, says if the branch was taken
 	input Clk,
 	input Rst, 
-	output [1:0] stateout // 2 bit predictor bits (current pediciton state)
+	output reg [1:0] stateout // 2 bit predictor bits (current pediciton state)
 );
 
 	// table of 2 bit states for each (of 128) local branch
@@ -82,16 +100,18 @@ module bht #(parameter ROWS = 32'h00000080) // 128 entries, 2^7
 	wire [6:0] table_offset;
 
 	// since byte addressable, ignore lower 2 bits because always 0
-	assign table_offset <= PC[8:2]; //seven bits to select one of the 2^7 table entries
+	assign table_offset = PC[8:2]; //seven bits to select one of the 2^7 table entries
 
 	parameter TAKEN  = 2'b00, // strongly taken
 	taken = 2'b01, // weakly taken
 	nottaken = 2'b10, // weakly not taken
 	NOTTAKEN = 2'b11; // stringly not taken
 
+	integer i;
 	// reset the branch history table
 	always @(posedge Rst)  
-	begin			
+	begin		
+		stateout <= 2'b11;	
 		for (i=0;i<ROWS;i=i+1)
 		begin
 			bhtable[i] <= 2'b11; //on a reset, default all to Strongly Not taken
@@ -104,11 +124,10 @@ module bht #(parameter ROWS = 32'h00000080) // 128 entries, 2^7
 	begin
 		// 'saturating counter' to implement states
 		case (bhtable[table_offset])
-		begin
-			TAKEN : bhtable[table_offset] <= (~branch) ? (taken) : (TAKEN);
-			taken : bhtable[table_offset] <= (~branch) ? (nottaken) : (TAKEN);
-			nottaken : bhtable[table_offset] <= (~branch) ? (NOTTAKEN) : (taken);
-			NOTTAKEN : bhtable[table_offset] <= (~branch) ? (NOTTAKEN) : (nottaken);
+			TAKEN : bhtable[table_offset] <= (~Branch) ? (taken) : (TAKEN);
+			taken : bhtable[table_offset] <= (~Branch) ? (nottaken) : (TAKEN);
+			nottaken : bhtable[table_offset] <= (~Branch) ? (NOTTAKEN) : (taken);
+			NOTTAKEN : bhtable[table_offset] <= (~Branch) ? (NOTTAKEN) : (nottaken);
 		endcase
 
 		// output the new state bits
--- Lab3/datapath.v
+++ Lab3/datapath.v
@@ -3,11 +3,12 @@ module datapath (input CLK, RESET);
 	// fetch and decode wires
 	wire [31:0] InstrF, PCPlus4F, PCBranchD, PCInter, PCJump, PCprime, PCF, InstrD, PCPlus4D;
 	wire PCSrcD, jumpD, StallF, FlushD, StallD, StallE, StallM;
-	wire [31:0] liljump;
+	wire [31:0] liljump, PCInter2, PCPredict;
 	wire ireadmiss, ireadready;
 	wire [31:0] iaddy;
 	wire [4095:0] idata;
 	wire predictionF;
+	wire [1:0] branchstate;
 	
 	// decode and execute wires
 	wire MemtoRegD, MemWriteD, RegDstD, RegWriteD, start_multD, mult_signD;
@@ -20,7 +21,7 @@ module datapath (input CLK, RESET);
 	wire ForwardAD, ForwardBD;
 	wire [31:0] RD1E, RD2E; 
 	wire [4:0] RsE, RtE, RdE;
-	wire FlushE;
+	wire FlushE, btbhit;
 	wire isBranchD, isBranchE, predictionD, PCSrcE;
 	wire [31:0] PCE, PCD, PCBranchE;
 
@@ -50,11 +51,13 @@ module datapath (input CLK, RESET);
 
 //-----------------FETCH----------------//
 	// PC Selection
-	mux2 branchmux( .d0(PCPlus4F) , .d1(PCBranchD), .s(PCSrcD), .y(PCInter));
-	mux2 jumpmux( .d0(PCInter), .d1(PCJump), .s(jumpD), .y(PCprime));
+	mux2 branchmux( .d0(PCPlus4F) , .d1(PCBranchD), .s((PCSrcD ^ predictionD)), .y(PCInter));
+	mux2 jumpmux( .d0(PCInter), .d1(PCJump), .s(jumpD), .y(PCInter2));
 
-	btbuff (.PC_current(PCF), .PC(PCE), .PCBranch(PCBranchE), .Branch(isBranchE), .BranchTaken(PCSrcE), .Clk(CLK), .Rst(RESET), 
-		.statein(branchstate), .PCPredict(), .prediction(predictionF)) //PCPredict is output to the new mux
+	mux2 predictmux( .d0(PCInter2), .d1(PCPredict), .s(btbhit), .y(PCprime));
+
+	btbuff btbuffer(.PC_current(PCF), .PC(PCE), .PCBranch(PCBranchE), .Branch(isBranchE), .BranchTaken(PCSrcE), .Clk(CLK), .Rst(RESET), 
+		.statein(branchstate), .PCPredict(PCPredict), .prediction(predictionF), .btbhit(btbhit)); //PCPredict is output to the new mux
 
 
         // pc register and instruction memory
@@ -64,7 +67,7 @@ module datapath (input CLK, RESET);
 			   .Rst(RESET), .Clk(CLK),
 			   .data(InstrF), .address(iaddy), .readmiss(ireadmiss));
 
-	inst_memory #(11) imem( .Address(iaddy), .Read_data(idata), .ReadReady(ireadready), .ReadMiss(ireadmiss), 
+	inst_memory #(42) imem( .Address(iaddy), .Read_data(idata), .ReadReady(ireadready), .ReadMiss(ireadmiss), 
 				.abort(1'b0), .Clk(CLK), .Rst(RESET));
 	adder plus4( .a(PCF), .b(32'b100), .y(PCPlus4F));
 
@@ -102,7 +105,7 @@ module datapath (input CLK, RESET);
 	mux2 branchfwdb( .d0(RD2D), .d1(ExecuteOutM), .s(ForwardBD), .y(branch_checkB));
 
 	// branch comparator
-	equality eq( .op(InstrD[31:26]), .srca(branch_checkA), .srcb(branch_checkB), .StallD(StallD), .eq_ne(PCSrcD), .branch());
+	equality eq( .op(InstrD[31:26]), .srca(branch_checkA), .srcb(branch_checkB), .StallD(StallD), .eq_ne(PCSrcD), .branch(isBranchD));
 	
 	// decode execute pipeline register, clear on a flush or reset
 	DEReg dereg( 	.RegWriteD(RegWriteD), .RegWriteE(RegWriteE), .MemtoRegD(MemtoRegD), .MemtoRegE(MemtoRegE), .MemWriteD(MemWriteD), 
@@ -111,12 +114,12 @@ module datapath (input CLK, RESET);
 			.MultSignE(mult_signE), .OutSelectD(out_selectD), .OutSelectE(out_selectE), .jumpD(jumpD), .jumpE(jumpE),
 			.Rd1D(RD1D), .Rd2D(RD2D), .Rd1E(RD1E), .Rd2E(RD2E), .RsD(InstrD[25:21]), .RsE(RsE), .RtD(InstrD[20:16]), 
 			.RtE(RtE), .RdD(InstrD[15:11]), . RdE(RdE), .SEimmD(SEimmD), .SEimmE(SEimmE), .ZEimmD(ZEimmD), .ZEimmE(ZEimmE),
-			.ZPimmD(ZPimmD), .ZPimmE(ZPimmE), .PCPlus4D(PCPlus4D), .PCPlus4E(PCPlus4E), .Clk(CLK), .Clr(FlushE || RESET), .En(StallE)
+			.ZPimmD(ZPimmD), .ZPimmE(ZPimmE), .PCPlus4D(PCPlus4D), .PCPlus4E(PCPlus4E), .Clk(CLK), .Clr(FlushE || RESET), .En(StallE),
 			.PCE(PCE), .PCD(PCD), .isBranchE(isBranchE), .isBranchD(isBranchD), .PCSrcE(PCSrcE), .PCSrcD(PCSrcD), .PCBranchE(PCBranchE), .PCBranchD(PCBranchD));
 
 //-----------------EXECUTE----------------//
 
-	bht bhtable(.PC(PCE), .Branch(isBranchE), .BranchTaken(PCSrcE), .Clk(CLK), .Rst(RESET), .stateout(brancstate));
+	bht bhtable(.PC(PCE), .Branch(isBranchE), .BranchTaken(PCSrcE), .Clk(CLK), .Rst(RESET), .stateout(branchstate));
 
 	// muxes for determining the destination register
 	mux2 #(5) regdest( .d0(RtE), .d1(RdE), .s(RegDstE), .y(prejumpWriteRegE));
